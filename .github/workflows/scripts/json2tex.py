79fad7e3e9b7ef01fbedc8dae8ac4ce179a53dee
Nathan Leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-10-16 09:32:00 -0500

    Update json2tex.py

--- .github/workflows/scripts/json2tex.py
+++ .github/workflows/scripts/json2tex.py
@@ -1,31 +1,48 @@
 import sys
 import json
-from typing import List, Dict
+from typing import List
 
-# Converts JSON structure to Markdown release notes.
+# Converts JSON structure to Latex release notes
 # Assumes a dictionary where keys are the release version, value of each key
-# is a list of strings.  Order of keys in file is the order the markdown will be generated.
-# The last argument can optionally match a key value and limit the output
-# to content in that key value.
+# is a list of strings.  Order of keys in file is the order the latex will be generated.
 
-def output_items(items : List[str], prefix="") -> None:
-  for item in items:
-    sys.stdout.write(f"{prefix}* {item}\n")
+def format_latex(text : str):
+  # Escape underscores
+  fixed = text.replace("_", "\\_")
 
-def output_dict(d : Dict) -> None:
-  for version in d:
-    sys.stdout.write(f"**{version}**\n")
-    output_items(d[version], "  ")
+  # Backticks => \texttt
+  converted = ""
+  toggle_off = False
+  for pos in range(0, len(fixed)):
+    if fixed[pos] == "`":
+      if toggle_off:
+        converted += "}"
+        toggle_off = False
+      else:
+        toggle_off = True
+        converted += "\\texttt{"
+    else:
+      converted += fixed[pos]
+
+  return converted
+
+
+
+def write_section(fp, version : str, items : List[str]) -> None:
+  fp.write(f"\\section{{{version}}}\n")
+  fp.write("\\begin{itemize}\n")
+  for line in items:
+    fp.write(f"\t\\item {format_latex(line)}\n")
+  fp.write("\\end{itemize}\n")
 
 if __name__ == "__main__":
   json_file_path = sys.argv[1]
+  latex_out_path = sys.argv[2]
 
   with open(json_file_path, "rt", encoding="UTF-8") as json_in:
     rn_json = json.load(json_in)
 
-  if len(sys.argv) > 2:
-    output_items(rn_json[sys.argv[2]])
-  else:
-    output_dict(rn_json)
 
-
+  with open(latex_out_path, "wt", encoding="UTF-8") as tex_out:
+    for version in rn_json:
+      write_section(tex_out, version, rn_json[version])
